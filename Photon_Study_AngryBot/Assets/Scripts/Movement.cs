356bc06a111e1304645a9eb4f7103c2a0b2bf634
Tunier <krymk1212@naver.com>
2021-09-29 17:47:56 +0900

    21.09.29

---
+++ Photon_Study_AngryBot/Assets/Scripts/Movement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    CharacterController controller;
+    Transform transform;
+    Animator animator;
+    Camera camera;
+
+    Plane plane;
+    Ray ray;
+    Vector3 hitPoint;
+
+    public float moveSpeed = 10f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        transform = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        camera = Camera.main;
+
+        // 가상의 바닥을 플레이어의 위치를 기준으로 생성.
+        plane = new Plane(transform.up, transform.position);
+    }
+
+    // 람다식은 표현을 간결하게 > 생산성을 증대시키는 구문
+    // 요즘에 많이 사용됨.
+    float h => Input.GetAxis("Horizontal");
+    float v => Input.GetAxis("Vertical");
+
+    void Move()
+    {
+        Vector3 cameraForward = camera.transform.forward;
+        Vector3 cameraRight = camera.transform.right;
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+
+        Vector3 moveDir = cameraForward * v + cameraRight * h;
+        moveDir.Set(moveDir.x, 0f, moveDir.z);
+
+        controller.SimpleMove(moveDir * moveSpeed);
+
+        float forward = Vector3.Dot(moveDir, transform.forward);
+        float strafe = Vector3.Dot(moveDir, transform.right);
+
+        animator.SetFloat("Forward", forward);
+        animator.SetFloat("Strafe", strafe);
+    }
+
+    void Turn()
+    {
+        ray = camera.ScreenPointToRay(Input.mousePosition);
+        float enter = 0f;
+        plane.Raycast(ray, out enter);
+        hitPoint = ray.GetPoint(enter);
+
+        Vector3 lookDir = hitPoint - transform.position;
+        lookDir.y = 0;
+        transform.localRotation = Quaternion.LookRotation(lookDir);
+    }
+
+    void Update()
+    {
+        Move();
+        Turn();
+    }
+}
