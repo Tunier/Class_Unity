9c7662a02d7fad975c5365e668a11fd06307fd9a
Tunier <krymk1212@naver.com>
2021-10-07 17:49:44 +0900

    21.10.07

---
+++ 21.09.30_UI_Study/Assets/SliderScr.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderScr : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SliderMove(float _f)
+    {
+        print(_f);
+    }
+}
---
+++ 21.09.30_UI_Study/Assets/btnScr.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnScr : MonoBehaviour
+{
+    public void btnPress()
+    {
+        print("버튼을 눌렀습니다.");
+    }
+}
